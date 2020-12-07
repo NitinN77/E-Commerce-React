5e78d400e250c809a6cdec14693ca5ffdf70f73d
Nitin N <nitinnarayanan77@gmail.com>
2020-12-07 11:20:32 +0530

    commjs

---
+++ src/components/Cart/Cart.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Container, Typography, Button, Grid} from '@material-ui/core';
+import classes from '*.module.css';
+
+
+function Cart() {
+    return (
+        <Container>
+            <div className={classes.toolbar} />
+        </Container>
+    )
+}
+
+
+
+export default Cart
