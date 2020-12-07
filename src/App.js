b7aa8b22cafc23b2866af9c1cbdfc98c553dc6ec
Nitin N <nitinnarayanan77@gmail.com>
2020-12-07 11:41:56 +0530

    cart

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { commerce } from './lib/commerce';
 
-import { Products, Navbar } from './components';
+import { Products, Navbar, Cart } from './components';
 
 function App() {
     const [products, setProducts] = useState([]);
@@ -32,6 +32,7 @@ function App() {
         <div>
             <Navbar totalItems={cart.total_items}/>
             <Products products={products} onAddToCart={handleAddToCart}/>
+            <Cart cart={cart}/>
         </div>
     );
 }
