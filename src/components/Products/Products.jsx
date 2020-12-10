349c6967b7f32a8ca9b9e01f59411e712c57f454
Nitin N <nitinnarayanan77@gmail.com>
2020-12-10 23:55:44 +0530

    product search bar

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -1,23 +1,37 @@
-import React from 'react';
-import { Grid } from '@material-ui/core';
-import Product from './Product/Product';
+import React, { useState } from "react";
+import { Grid } from "@material-ui/core";
+import Product from "./Product/Product";
+import SearchBar from "material-ui-search-bar";
 
-import useStyles from './styles';
+import useStyles from "./styles";
 import { useStateValue } from "../../StateProvider";
 
 export default function Products() {
-    const [{ products }] = useStateValue();
-    const classes = useStyles();
-    return (
-        <main className={classes.content}>
-            <div className={classes.toolbar} />
-            <Grid container justify="center" spacing={4}>
-                {products.map((product) => (
-                    <Grid item key={product.id} xs={12} sm={6} md={4} lg={3}>
-                        <Product product={product} />
-                    </Grid>
-                ))}
-            </Grid>
-        </main>
-    )
+  const [{ products }] = useStateValue();
+  const classes = useStyles();
+  const [searchTerm, setSearchTerm] = useState("");
+
+  const dynamicSearch = () => {
+    return products.filter((product) =>
+      product.name.toLowerCase().includes(searchTerm.toLowerCase())
+    );
+  };
+
+  return (
+    <main className={classes.content}>
+      <div className={classes.toolbar} />
+      <SearchBar
+        value={searchTerm}
+        onChange={(val) => setSearchTerm(val)}
+      />
+      <br />
+      <Grid container justify="center" spacing={4}>
+        {dynamicSearch().map((product) => (
+          <Grid item key={product.id} xs={12} sm={6} md={4} lg={3}>
+            <Product product={product} />
+          </Grid>
+        ))}
+      </Grid>
+    </main>
+  );
 }
